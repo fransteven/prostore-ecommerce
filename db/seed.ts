26e555efb68c13830fea22bdc86506b01da52da3
fransteven <fransteven18@hotmail.com>
2026-04-10 08:30:56 -0500

    Sign In & Sign Out Server Actions

--- db/seed.ts
+++ db/seed.ts
@@ -7,8 +7,17 @@ async function main() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
     const prisma = new PrismaClient({ adapter });
     await prisma.product.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+
+
     await prisma.product.createMany({
         data: sampleData.products,
+    });
+    await prisma.user.createMany({
+        data: sampleData.users,
     });
 }
 
