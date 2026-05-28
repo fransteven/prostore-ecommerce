a8efc9bb32155d1fc0389491ab9cc3bbc2292f2e
fransteven <fransteven18@hotmail.com>
2026-05-28 07:37:07 -0500

    Price Calc For Cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,10 +2,26 @@
 
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
+import { Prisma } from "@/lib/generated/prisma";
 import { CartItem } from "@/types";
 import { cookies } from "next/headers";
-import { convertToPlainObject } from "../utils";
-import { cartItemSchema } from "../validators";
+import { convertToPlainObject, round2 } from "../utils";
+import { cartItemSchema, insertCartSchema } from "../validators";
+import { revalidatePath } from "next/cache";
+
+// Calculate cart Prices
+const calcPrice = (items: CartItem[]) =>{
+  const itemsPrice = round2(items.reduce((acc, item)=>acc + Number(item.price)*item.qty, 0)),
+  shippingPrice = round2(itemsPrice >100?0:10),
+  taxPrice = round2(0.15 * itemsPrice),
+  totalPrice = round2(itemsPrice + shippingPrice + taxPrice)
+
+  return{
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2)}
+}
 
 export const addItemToCart = async (data: CartItem) => {
   try {
@@ -28,13 +44,29 @@ export const addItemToCart = async (data: CartItem) => {
         where: {id: item.productId}
     })
 
-    //Testing
-    console.log({
-      "Session Cart ID": sessionCartId,
-      "User ID": userId,
-      "Item Requested": item,
-      "Product": product,
-    });
+    if(!product) throw new Error("Product not found")
+
+    if(!cart){
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId:userId,
+        sessionCartId:sessionCartId,
+        items: [item],
+        ...calcPrice([item])
+      })
+      // Add to database
+      await prisma.cart.create({data: newCart as Prisma.CartUncheckedCreateInput})
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`)
+
+      return {
+        success: true,
+        message: `${product.name} added to cart`
+      }
+    }else{
+
+    }
+
   } catch (error) {
     console.error("Error adding item to cart:", error);
     return { success: false, message: "Something went wrong!" };
