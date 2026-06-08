8062db1f0381925502c63242056d19a18ccb521f
fransteven <fransteven18@hotmail.com>
2026-06-08 00:18:29 -0500

    Shipping Address Zod Schema & Page

---
+++ app/(root)/shipping-address/page.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Shipping Address",
+};
+
+async function ShippingAddressPage() {
+    const cart = await getMyCart()
+
+    if(!cart || cart.items.length === 0) redirect('/cart')
+
+    const session = await auth()
+
+    const userId = session?.user?.id
+
+    if(!userId) throw new Error('No user ID')
+
+    const user = await getUserById(userId)
+
+
+  return <div>ShippingAddressPage</div>;
+}
+
+export default ShippingAddressPage;
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -21,3 +21,11 @@ export const sigUpDefaultValues = {
   password:'',
   confirmPassword:''
 }
+
+export const shippingAddressDefaultValues = {
+  fullName: 'John Doe',
+  streetAddress: '123 Main st',
+  city: 'Anytown',
+  postalCode: '12345',
+  country: 'USA',
+}
