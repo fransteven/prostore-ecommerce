b4f64304987ed55a2915d0b3bc22055b4e3d1101
fransteven <fransteven18@hotmail.com>
2026-06-28 01:17:06 -0500

    Order Zod Schemas & Types

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { number, z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 import { PAYMENT_METHODS } from "./constants";
 
@@ -69,4 +69,26 @@ export const shippingAddressSchema = z.object({
 export const paymentMethodSchema = z.object({type: z.string().min(1, 'Payment method is required')}).refine((data)=>PAYMENT_METHODS.includes(data.type),{
     path:['type'],
     message: 'Invalid payment method'
+})
+
+// Schema for inserting order
+export const insertOrderSchema = z.object({
+    userId: z.string().min(1,"User is required"),
+    itemsPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    totalPrice: currency,
+    paymentMethod: z.string().refine((data)=>PAYMENT_METHODS.includes(data),{message:"Invalid payment method"}),
+    shippingAddress: shippingAddressSchema
+})
+
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+    productId: z.string(),
+    slug: z.string(),
+    image: z.string(),
+    name: z.string(),
+    price: currency,
+    qty: z.number()
 })
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { cartItemSchema, insertCartSchema, insertProductSchema, shippingAddressSchema } from "@/lib/validators";
+import { cartItemSchema, insertCartSchema, insertOrderItemSchema, insertOrderSchema, insertProductSchema, shippingAddressSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -10,3 +10,15 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>
 export type CartItem = z.infer<typeof cartItemSchema>
 export type ShippingAddressType = z.infer<typeof shippingAddressSchema>
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string
+    createdAt: Date
+    isPaid: boolean
+    paidAt: Date | null
+    isDelivered: boolean
+    deliveredAt: Date | null
+    orderitems: OrderItem[]
+    user: {name:string; email:string}
+
+}
