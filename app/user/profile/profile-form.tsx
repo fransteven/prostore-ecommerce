a3b679697b52cd424972274fc8b6b829d28c9fb7
fransteven <fransteven18@hotmail.com>
2026-08-06 09:09:41 -0500

    Profile Form Submission

--- app/user/profile/profile-form.tsx
+++ app/user/profile/profile-form.tsx
@@ -2,10 +2,12 @@
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { updateProfile } from "@/lib/actions/user.actions";
 import { updateProfileSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 export default function ProfileForm() {
@@ -18,8 +20,23 @@ export default function ProfileForm() {
     }
   })
 
-  const onSubmit = () =>{
-    return;
+  const onSubmit = async (values: z.infer<typeof updateProfileSchema>) =>{
+    const res = await updateProfile(values)
+    if(!res?.success){
+        return toast.message(res?.message)
+    }
+
+    const newSession = {
+        ...session,
+        user: {
+            ...session?.user,
+            name: values.name
+        }
+    }
+
+    await update(newSession)
+
+    return toast.message(res.success)
   }
 
   return <Form {...form}>
