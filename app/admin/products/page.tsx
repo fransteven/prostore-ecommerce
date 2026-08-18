519507909c5c7246678b9cdfa76ecf058e5ae512
fransteven <fransteven18@hotmail.com>
2026-08-17 23:09:36 -0500

    Get Products For Admin Page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,38 @@
+import { getAllProducts } from "@/lib/actions/product.actions";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+
+
+async function AdminProductsPage(
+    props: {
+        searchParams: Promise<{
+            page: string,
+            query: string,
+            category: string,
+        }>
+    }
+) {
+
+    const searchParams = await props.searchParams
+    const page = Number(searchParams.page) || 1
+    const searchText = searchParams.query || ""
+    const category = searchParams.category || ""
+
+    const products = await getAllProducts({
+        query:searchText,
+        page,
+        category
+    })
+
+    console.log(products)
+    
+    
+    return (
+        <div className="space-y-2">
+            <div className="flex-between">
+                <h1 className="h2-bold">Products</h1>
+            </div>
+        </div>
+    )
+}
+
+export default AdminProductsPage
